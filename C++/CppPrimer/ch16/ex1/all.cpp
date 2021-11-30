26fc847c7afd5bafb6d37dcb6d410eb3e1cddde3
xiaguan <751080330@qq.com>
2021-11-30 20:47:58 +0800

    啊啊

---
+++ C++/CppPrimer/ch16/ex1/all.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+// 求数组长度
+template <typename  T,size_t N> constexpr
+int func(const T (&t)[N]){
+    return N;
+}
+
+// 数组begin和end的实现
+template <typename T>
+T* begin(const T &t){return &t;}
+
+template <typename T,size_t N>
+T* end(const T (&t)[N]){return &t[N];}
+
+// 为什么C++程序员更喜欢用!=而不喜欢< ，并且迭代器不喜欢下标
+// 因为这种编程风格再标准库提供的所有容器上都有效
---
+++ C++/CppPrimer/ch16/ex1/blob.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+template <typename T> class Blob {
+public:
+    typedef T value_type;
+    typedef typename std::vector<T>::size_type size_type;
+    Blob();
+    Blob(std::initializer_list<T> il);
+    size_type size() const { return data->size(); }
+    bool empty() const { return data->empty(); }
+    void push_back(const T& t) { data->push_back(t); }
+    void push_back(T&& t) { data->push_back(std::move(t)); }
+    void pop_back();
+    T& back();
+    T& operator[](size_type i);
+private:
+    std::shared_ptr<std::vector<T>> data;
+    void check(size_type i, const std::string& msg) const;
+};
+
+int main() {
+    Blob<int> ia2 = { 0,1,2,3,4,5 };
+}
+
